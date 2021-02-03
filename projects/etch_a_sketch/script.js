a4ad4aa06e6933b3529409283d8f333bc438882c
maplesyrupman <maplesyrupman@protonmail.com>
2021-02-02 23:02:30 -0500

    random colour onmouseover, shades to black over 5 passes

--- projects/etch_a_sketch/script.js
+++ projects/etch_a_sketch/script.js
@@ -8,7 +8,7 @@ function resetNewDimensions() {
     let newDimensions;
     do {
         newDimensions = Number(prompt('Enter a number between 10 and 100 to create a new sketch pad with those dimensions'));
-    } while (typeof newDimensions != 'number' || newDimensions > 100 || newDimensions < 10);
+    } while (isNaN(newDimensions) || newDimensions > 100 || newDimensions < 10);
     clearContainer();
     createGrid(newDimensions);
     addDivs(newDimensions);
@@ -32,18 +32,41 @@ function addDivs(dimensions) {
         const div = document.createElement('div');
         div.setAttribute('id', `${i}`);
         div.classList.add('pixels');
+        div.dataset.lightness = 50;
         theGrid.appendChild(div);
     }
     const pixels = document.querySelectorAll('.pixels');
 
     pixels.forEach((element) => {
+        const thisID = element.id;
         element.addEventListener('mouseover', (e) =>{
-        console.log(e.target.style.backgroundColor);
-
-        e.target.style.backgroundColor = 'blue';
+            console.log(e.target.id)
+            e.target.style.backgroundColor = setColour(e.target.id);
+            
         
     });
 });
+}
+
+function getRandomColour() {
+    const colours = [0, 30, 60, 120, 240, 285]; //red, orange, yellow, green, blue, purple
+    colour = colours[Math.floor(Math.random() *6)];
+    return colour;
+}
+
+function setColour(divID) {
+    const divToColour = document.getElementById(divID);
+    let lightness = divToColour.dataset.lightness;
+    if (lightness == 50) {
+        hslColour = `hsl(${getRandomColour()}, 100%, ${lightness}%)`;
+        divToColour.dataset.lightness = lightness - 10;
+        return hslColour;
+    } else if (lightness > 0) {
+        divToColour.dataset.lightness = divToColour.dataset.lightness - 10;
+        lightness = divToColour.dataset.lightness;
+        hslColour = `hsl(${getRandomColour()}, 100%, ${lightness}%)`;
+        return hslColour;
+    }
 }
 
 createGrid(16);
